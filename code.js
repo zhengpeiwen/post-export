5b55a5249129946d6f3fc421c8a1ce57489104da
zhengpeiwen <821894829@qq.com>
2021-02-19 10:57:11 +0800

    first commit

---
+++ code.js
@@ -0,0 +1,28 @@
+export function exportFileUtils(params, url, filename) {
+  axios({
+    method: 'post',
+    withCredentials: true,
+    url: url, // 请求地址
+    data: params,  //  传参
+    responseType: 'blob', // 防止导出文件破损或乱码
+    headers: {
+      'Content-Type': 'application/json',
+      'x-auth-token': userInfo.token,  // token
+      'x-project-id': userInfo.projectId  //  项目id  
+      }
+  }).then((res) => {
+    try {
+      var blob = new Blob([res.data]);
+      var filename = '导出文件.xlsx';  //  导出文件名字
+      var a = document.createElement('a');
+      var url = window.URL.createObjectURL(blob);
+      a.href = url;
+      a.download = filename || '';
+      var body = document.getElementsByTagName('body')[0];
+      body.appendChild(a);
+      a.click();
+      body.removeChild(a);
+      window.URL.revokeObjectURL(url);
+    } catch (e) {}
+  });
+}
